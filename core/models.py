a7798e5b05c514d5f25f4100e1e78a55020650d0
Renan da Silva Oliveira Andrade <renan.silva3@pucpr.edu.br>
2024-09-04 10:20:57 -0300

    continue models

--- core/models.py
+++ core/models.py
@@ -25,6 +25,8 @@ class User(Base):
         back_populates="user", cascade="all, delete-orphan",
     )
 
+    rescues : Mapped[Set["Rescue"]] = relationship(back_populates="user")
+
     def __repr__(self):
         return f"""
         User (
@@ -39,6 +41,101 @@ class User(Base):
         )
         """
 
+class Cat(Base):
+    __tablename__ = "cats"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    name : Mapped[str] = mapped_column(String(50))
+    age : Mapped[int] = mapped_column(SmallInteger)
+    sex : Mapped[str] = mapped_column(CHAR(1))
+
+    def __repr__(self):
+        return f"""
+        Cat (
+            id={self.id!r},
+            name={self.name!r},
+            age={self.age!r},
+            sex={self.sex!r}
+        )
+        """
+
+class Disease(Base):
+    __tablename__ = "diseases"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    name : Mapped[str] = mapped_column(String(50))
+    description : Mapped[Optional[str]] = mapped_column(String(1024))
+
+    vaccine : Mapped[Optional["Vaccine"]] = relationship(back_populates="diseases")
+
+    def __repr__(self):
+        return f"""
+        Disease (
+            id={self.id!r},
+            name={self.name!r},
+            description={self.description!r}
+        )
+        """
+    
+class Vaccine(Base):
+    __tablename__ = "vaccines"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    name : Mapped[str] = mapped_column(String(50))
+    description : Mapped[Optional[str]] = mapped_column(String(1024))
+    disease_id : Mapped[int] = mapped_column(ForeignKey("diseases.id"))
+
+    diseases : Mapped[Set["Disease"]] = relationship(back_populates="vaccine")
+
+    def __repr__(self):
+        return f"""
+        Vaccine (
+            id={self.id!r},
+            name={self.name!r},
+            description={self.description!r},
+            disease_id={self.disease_id!r}
+        )
+        """
+
+class Personality(Base):
+    __tablename__ = "personalities"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    name : Mapped[str] = mapped_column(String(30))
+    description : Mapped[str] = mapped_column(String(1024))
+
+    user_preferences : Mapped[Optional[Set["UserPreference"]]] = relationship(
+        back_populates="personality", cascade="all delete-orphan",
+    )
+
+    def __repr__(self):
+        return f"""
+        Personality (
+            id={self.id!r},
+            name={self.name!r},
+            description={self.description!r}
+        )
+        """
+
+class Color(Base):
+    __tablename__ = "colors"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    name : Mapped[str] = mapped_column(String(10))
+
+    user_preferences : Mapped[Optional[Set["UserPreference"]]] = relationship(
+        back_populates="cat_color",
+        cascade="all, delete-orphan",    
+    )
+
+    def __repr__(self):
+        return f"""
+        Color (
+            id={self.id!r},
+            name={self.name!r}
+        )
+        """
+
 class UserPreference(Base):
     __tablename__ = "user_preferences"
 
@@ -47,8 +144,11 @@ class UserPreference(Base):
     personality_id : Mapped[int] = mapped_column(ForeignKey("personalities.id"))
     age : Mapped[Optional[int]] = mapped_column(SmallInteger)
 
-    user : Mapped["User"] = relationship(
-        back_populates="user_preferences",
+    user : Mapped["User"] = relationship(back_populates="user_preferences")
+    color : Mapped["Color"] = relationship(back_populates="user_preferences")
+
+    personality : Mapped["Personality"] = relationship(
+        back_populates="preferences",
     )
 
     def __repr__(self):
@@ -61,3 +161,30 @@ class UserPreference(Base):
         )
         """
 
+class Rescue(Base):
+    __tablename__ = "rescues"
+
+    user_id : Mapped[int] = mapped_column(ForeignKey("users.id"))
+    request_datetime : Mapped[datetime] = mapped_column(DateTime)
+    status : Mapped[str] = mapped_column(String(50))
+    description : Mapped[str] = mapped_column(String(1024))
+    addr_city : Mapped[str] = mapped_column(String(50))
+    addr_state : Mapped[str] = mapped_column(String(100))
+    addr_number : Mapped[int] = mapped_column(Integer)
+    addr_zipcode : Mapped[str] = mapped_column(CHAR(8))
+
+    user : Mapped["User"] = relationship(back_populates="rescues")
+
+    def __repr__(self):
+        return f"""
+        Rescue (
+            user_id={self.user_id!r},
+            request_datetime={self.request_datetime!r},
+            status={self.status!r},
+            description={self.description!r},
+            addr_city={self.addr_city!r},
+            addr_state={self.addr_state!r},
+            addr_number={self.addr_number!r},
+            addr_zipcode={self.addr_zipcode!r}
+        )
+        """
